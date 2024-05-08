410acea6cc1c3f44b43b41767cead13f432d8571
Farhad Bagheri <coding.farhad@gmail.com>
2024-05-08 11:51:17 +0330

    update

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/plugins/global/plugins.bundle.css',
-                'resources/plugins/global/plugins.bundle.rtl.css',
                 'resources/js/app.js',
                 'resources/js/create-fast-category.js'
             ],
